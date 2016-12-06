3bf75858a4d9352b360fe420a5308965a0b0fb9e
joshuaswan <joshuaswan@sina.com>
2016-12-06 16:48:49 +0800

    [joshua]create the project, finish the static part.

---
+++ app.js
@@ -0,0 +1,43 @@
+var PORT = 8980;
+
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
+var path = require("path");
+var mime = require("./mime").types;
+
+var server = http.createServer(function(request,response){
+	//TODO
+	var pathname = url.parse(request.url).pathname;
+
+	var realPath = "assets" + pathname;
+
+	path.exists(realPath,function(exists){
+
+		if(!exists){
+			response.writeHead(404,{'Content-Type':'text/plain'});
+			response.write("This request URL " + pathname + " was not found on this server.");
+
+			response.end();
+		}else{
+			fs.readFile(realPath,"binary",function(err,file){
+				if(err){
+					response.writeHead(500,{'Content-Type':'text/plain'});
+					response.end();
+				}else{
+					var ext = path.extname(realPath);
+					ext = ext ? ext.slice(1) : 'unknown';
+					var contentType = mime[ext] || "text/plain";
+					response.writeHead(200,{'Content-Type':contentType});
+					response.write(file,"binary");
+					response.end();
+				}
+			});
+		}
+	});
+
+});
+
+server.listen(PORT);
+
+console.log("Server runing at port: " + PORT + ".");
